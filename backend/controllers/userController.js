1871620e3123054f98196d6163ebe784778f2cf0
Aryan Agarwal <aryanaggarwal1901@gmail.com>
2024-02-25 01:41:11 -0800

    login-functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,26 +1,52 @@
-import {catchAsyncErrors} from "../middlewares/catchAsyncErrors"
-import ErrorHandler from "../middlewares/error.js"
-import {User} from '../models/userSchema.js'
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
+import ErrorHandler from "../middlewares/error.js";
+import { User } from "../models/userSchema.js";
 
-export const register= catchAsyncErrors(async (req,res,next)=>{
-    const {name, email, password, phone, role, education}= req.body;
-    if(!name || !email || !password || !phone || !role|| !education){
-        return next(new ErrorHandler("Please fill full details!", 400));
-    }
-    const user= await User.findOne({email});
-    if(user){
-        return next(new ErrorHandler("User already exists",400));
-    }
-    await User.create({
-        name, 
-        email, 
-        password, 
-        phone, 
-        role, 
-        education,
-    });
-    res.status(200).json({
-        success: true,
-        message: "User registered!",
-    });
+export const register = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password, phone, role, education } = req.body;
+  if (!name || !email || !password || !phone || !role || !education) {
+    return next(new ErrorHandler("Please fill full details!", 400));
+  }
+  const user = await User.findOne({ email });
+  if (user) {
+    return next(new ErrorHandler("User already exists", 400));
+  }
+  await User.create({
+    name,
+    email,
+    password,
+    phone,
+    role,
+    education,
+  });
+  res.status(200).json({
+    success: true,
+    message: "User registered!",
+  });
+});
+
+export const login = catchAsyncErrors(async (req, res, next) => {
+  const { email, password, role } = req.body;
+  if (!email || !password || !role) {
+    return next(new ErrorHandler("Please fill full form", 400));
+  }
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 400));
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email or Password"), 400);
+  }
+  if (user.role !== role) {
+    return next(
+      new ErrorHandler(`User with provided role(${role}) not found`, 400)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    message: "User logged in!",
+  });
 });
--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -9,41 +10,53 @@ const userSchema = new mongoose.Schema({
     maxLength: [32, "Name cannot exceed 32 characters!"],
   },
   email: {
-    type:String,
-    required:true,
-    validate: [validator.isEmail, "Please provide a valid email"]
+    type: String,
+    required: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
   },
   phone: {
-    type:Number,
-    required:true,
+    type: Number,
+    required: true,
   },
   avatar: {
-    public_id:{
-        type:String
+    public_id: {
+      type: String,
     },
-    url:{
-        type:String
-    }
+    url: {
+      type: String,
+    },
   },
   education: {
-    type:String,
-    required:true,
+    type: String,
+    required: true,
   },
   role: {
-    type:String,
-    required:true,
-    enum:["Reader","Author"]
+    type: String,
+    required: true,
+    enum: ["Reader", "Author"],
   },
   password: {
-    type:String,
-    required:true,
+    type: String,
+    required: true,
     minLength: [8, "Password must contain atleast 8 characters!"],
     maxLength: [32, "Password cannot exceed 32 characters!"],
+    select: false,
   },
   createdOn: {
-    type:Date,
+    type: Date,
     default: Date.now,
   },
 });
 
-export const User= mongoose.model("User",userSchema)
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function(enteredPassword){
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
